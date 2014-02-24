aec16a6f7fb02365e1aa691b489f00ae3c2ada50
Jay Hendren <james.g.hendren@gmail.com>
2014-02-24 16:54:53 -0700

    tweaked build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 apt-get install -y software-properties-common python-software-properties python g++ make
 add-apt-repository ppa:chris-lea/node.js
-apt-get update
-apt-get install nodejs npm
+apt-get update -y
+apt-get install -y nodejs npm
 npm config set registry http://registry.npmjs.org/
 npm install
