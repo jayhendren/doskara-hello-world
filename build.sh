9e925997334bd472e96b2b9a2ba23ecb46cb9f60
Jay Hendren <james.g.hendren@gmail.com>
2014-02-24 16:54:17 -0700

    tweaked build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-apt-get install python-software-properties python g++ make
+apt-get install -y software-properties-common python-software-properties python g++ make
 add-apt-repository ppa:chris-lea/node.js
 apt-get update
 apt-get install nodejs npm
