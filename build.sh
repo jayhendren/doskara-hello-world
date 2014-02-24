8c3857674e74e385e1627d2d743afad0285d5c1e
Jay Hendren <james.g.hendren@gmail.com>
2014-02-24 16:52:58 -0700

    updated build script to build latest version of node

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
+apt-get install python-software-properties python g++ make
+add-apt-repository ppa:chris-lea/node.js
+apt-get update
+apt-get install nodejs npm
+npm config set registry http://registry.npmjs.org/
 npm install
